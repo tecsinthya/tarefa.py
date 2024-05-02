eff1a69c63f6b60981d4aed056b809037613d3e0
tecsinthya <137504450+tecsinthya@users.noreply.github.com>
2024-05-02 09:12:04 -0300

    Add files via upload

---
+++ listaLivros.js
@@ -0,0 +1,28 @@
+const precoLivros = [
+    {        
+        titulo: "javaScript",
+        preco: 25
+    },
+    {
+        titulo: "PHP",
+        preco: 15
+    },
+    {
+        titulo: "java",
+        preco: 30
+    },
+    {
+        titulo: "Elixir",
+        preco: 50
+    },
+    {
+        titulo: "Go",
+        preco: 45
+    },
+    {
+        titulo: "python",
+        preco: 20
+    }, 
+    
+];
+module.exports = precoLivros;
